bf3904fa25b5366064eefb1a7112af154deb2fb0
Qn <qina6050@163.com>
2020-03-18 16:43:57 +0800

    修复报错

--- src/utils/scrollbar.js
+++ src/utils/scrollbar.js
@@ -11,17 +11,20 @@ import PerfectScrollbar from 'perfect-scrollbar'
 import 'perfect-scrollbar/css/perfect-scrollbar.css'
 
 const el_scrollBar = (el) => {
+    // 名字随便取，确保不会和已有属性重复即可，取名叫做_ps_
     if (el._ps_ instanceof PerfectScrollbar) {
         el._ps_.update()
     } else {
+        //el上挂一份属性
         el._ps_ = new PerfectScrollbar(el, {suppressScrollX: true})
     }
 };
-
+//接着，自定义Vue指令,指令名你自己随便编一个，scrollBar
 Vue.directive('scrollBar', {
+//使用inserted钩子函数（初次创建dom）获取使用自定义指令处的dom
     inserted(el, binding, vnode) {
         const rules = ['fixed', 'absolute', 'relative'];
-        let position = window.getComputedStyle(el).position;
+        let position = window.getComputedStyle(el,null).position;
         if (
             position != 'fixed' &&
             position != 'absolute' &&
@@ -34,9 +37,11 @@ Vue.directive('scrollBar', {
         el_scrollBar(el)
     },
 
+    //更新dom的时候
     componentUpdated(el, binding, vnode, oldVnode) {
-        let content = document.getElementById('app-content');
+        let content = document.getElementById('app');
         content.scrollTop = 0;
+        console.log('content',content)
         try {
             vnode.context.$nextTick(() => {
                 el_scrollBar(el)
